d9293b86d59ca0758f22d56c1cc6d2b03e247e96
Vinicius <vncastanheira@gmail.com>
2019-02-06 19:10:26 -0200

    q5

--- src/main/java/challenge/Main.java
+++ src/main/java/challenge/Main.java
@@ -7,17 +7,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    String[] tableColumns;
-    ArrayList<String[]> table;
+    private String[] tableColumns;
+    private ArrayList<String[]> table;
 
     public static void main(String[] args){
         Main main = new Main();
@@ -28,15 +39,26 @@ public class Main {
         //int clubs = main.q2();
         //System.out.println(clubs +" clubs found.");
 
-        List<String> names = main.q3();
-        System.out.println(names.size() + " names found.");
-        System.out.println(names);
+        //List<String> names = main.q3();
+        //System.out.println(names.size() + " names found.");
+        //System.out.println(names);
+
+//        List<String> bigClausesNames = main.q4();
+//        if(bigClausesNames == null) return;
+//        System.out.println(bigClausesNames.size() + " names found.");
+//        System.out.println(bigClausesNames);
+
+        List<String> bigClausesNames = main.q5();
+        if(bigClausesNames == null) return;
+        System.out.println(bigClausesNames.size() + " names found.");
+        System.out.println(bigClausesNames);
     }
 
-    public void processFile() throws NullPointerException, FileNotFoundException, IOException {
+    public void processFile() throws NullPointerException, IOException {
 
             URL url = getClass().getClassLoader().getResource("data.csv");
-            File file = new File(url.getFile());
+            String fileUrl = url.getFile();
+            File file = new File(fileUrl);
 
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -52,6 +74,16 @@ public class Main {
             }
             System.out.println(String.format("Read %s entries", table.size()));
 
+    }
+
+
+    private int getColumnIndex(String columnName)throws Exception {
+        int index = Arrays.asList(tableColumns).indexOf(columnName);
+        if(index < 0){
+            throw new Exception(columnName + " not found.");
+        }
+
+        return index;
     }
 
     // Quantas nacionalidades (coluna `nationality`) diferentes existem no arquivo?
@@ -81,12 +113,7 @@ public class Main {
 	public int q2() {
         try {
             processFile();
-            int clubIndex = Arrays.asList(tableColumns).indexOf("club");
-            if(clubIndex < 0)
-            {
-                System.out.println("club not found");
-                return -1;
-            }
+            int clubIndex = getColumnIndex("club");
 
             List<String> clubs = table.stream().map(p -> p[clubIndex])
                     .filter(c -> !c.isEmpty())
@@ -111,10 +138,8 @@ public class Main {
                 return null;
             }
 
-            List<String> names = table.subList(0, 20).stream().map(p -> p[full_nameIndex])
+            return table.subList(0, 20).stream().map(p -> p[full_nameIndex])
                     .collect(Collectors.toList());
-
-            return names;
         }
         catch (Exception e){
             System.out.println(e.getMessage());
@@ -125,13 +150,87 @@ public class Main {
     // (utilize as colunas `full_name` e `eur_release_clause`)
 
 	public List<String> q4() {
-		return null;
+		try {
+		    processFile();
+		    int clauseIndex = Arrays.asList(tableColumns).indexOf("eur_release_clause");
+		    if(clauseIndex < 0)
+            {
+                System.out.println("eur_release_clause not found");
+                return null;
+            }
+
+            int full_nameIndex = Arrays.asList(tableColumns).indexOf("full_name");
+            if(full_nameIndex < 0)
+            {
+                System.out.println("club not found");
+                return null;
+            }
+
+            Stream<String[]> playerStream = table.stream().filter(p -> !p[clauseIndex].isEmpty()).sorted((p1, p2) -> {
+                double p1Clause = Double.parseDouble(p1[clauseIndex]);
+                double p2Clause = Double.parseDouble(p2[clauseIndex]);
+                return Double.compare(p2Clause, p1Clause);
+            });
+
+            return playerStream.limit(10).map(p -> p[full_nameIndex]).collect(Collectors.toList());
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
 	}
     // Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
     // (utilize as colunas `full_name` e `birth_date`)
-
 	public List<String> q5() {
-		return null;
+		try {
+            processFile();
+            int bithDateIndex = getColumnIndex("birth_date");
+            int wageIndex = getColumnIndex("eur_wage");
+            int full_nameIndex = getColumnIndex("full_name");
+
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+            Stream<String[]> playerStream = table.stream().filter(p -> !p[bithDateIndex].isEmpty()).sorted((p1, p2) -> {
+                try {
+                    Date p1Birth = df.parse(p1[bithDateIndex]);
+                    Date p2Birth = df.parse(p2[bithDateIndex]);
+                    int comparison = p1Birth.compareTo(p2Birth);
+                    if (comparison == 0) {
+                        Double p1Wage = Double.parseDouble(p1[wageIndex]);
+                        Double p2Wage = Double.parseDouble(p2[wageIndex]);
+                        return Double.compare(p1Wage, p2Wage);
+                    }
+                    else {
+                        return comparison;
+                    }
+                }
+                catch (ParseException e){
+                    System.out.println("Error parsing birthday dates.");
+                    return 0;
+                }
+            });
+
+            return playerStream.limit(10).map(p -> {
+                try{
+                    Date today = new Date();
+                    Date d = df.parse(p[bithDateIndex]);
+                    long diff = today.getTime() - d.getTime();
+                    long years = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) / 365;
+
+                    return p[full_nameIndex] + " - " + years + "yo";
+                }
+                catch (ParseException e){
+                    System.out.println("Error parsing birthday dates.");
+                    return "<null>";
+                }
+
+            }).collect(Collectors.toList());
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
 	}
     // Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
     // (utilize a coluna `age`)
